8b8cb93ea9e7c092a2971f5d2fdb567592ef2e84
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-06-20 17:03:20 +0300

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const PORT = 8000;
 const express = require("express");
 const app = express();
+const connectDb = require("./database");
+connectDb();
+
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
