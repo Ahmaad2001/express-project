ab90751d1687a8d1481a3dc40dcbf24a8fb06854
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-06-20 17:28:12 +0300

    4th commit, middlewares done

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const app = express();
 const connectDb = require("./database");
 connectDb();
 const morgan = require("morgan");
+const errorHandler = require("./middlewares/errorHandler");
+const notFoundHandler = require("./middlewares/notFoundHandler");
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
