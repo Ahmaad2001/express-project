13e66c873a23d2e2cfee7c6c5ce0bf360fae6a50
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-06-20 17:19:05 +0300

    3rd commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const connectDb = require("./database");
 connectDb();
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.listen(PORT, () => {
